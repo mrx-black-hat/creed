1056dd5e792d3db74aa658700d98e661d05a7503
mrx-black-hat <amirmohammadmozafari79@gmail.com>
2016-03-11 14:27:27 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -243,12 +243,12 @@ function create_config( )
     
      Hello my Good friends 
      
-    ‼️ this bot is made by : @creed_is_dead
+    ‼️ this bot is made by : @mr_x_black_hat
    〰〰〰〰〰〰〰〰
    ߔࠀ   our admins are : 
-   ߔࠀ   @sorblack_creed
-   ߔࠀ   @amircc_creed
-   ߔࠀ   @aria_creed
+   ߔࠀ   @coming soon
+   ߔࠀ   @coming soon
+   ߔࠀ   @coming soon
    〰〰〰〰〰〰〰〰
   ♻️ You can send your Ideas and messages to Us By sending them into bots account by this command :
    تمامی درخواست ها و همه ی انتقادات و حرفاتونو با دستور زیر بفرستین به ما
